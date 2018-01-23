01290047bc625938fa1fd06195d629356e0cc134
cpenalosa <penalosa619@gmail.com>
2018-01-23 14:18:46 -0500

    Update vehicle-track-importer.js

--- vehicle-track-importer.js
+++ vehicle-track-importer.js
@@ -1,16 +1,12 @@
 // https://github.com/cpenalosa/testgps/blob/master/test-data.csv
  
-
 export default (row) => {
-  if (row.entry_id == null || row.entry_id === 0) return null
-  if (row.field1 == null || row.field1 === 0) return null
-  if (row.field2 == null || row.field2 === 0) return null
   return {
-    id: entry_id,
-    utc-time: created_at,
+    id: row.entry_id,
+    time: row.created_at,
     location: {
       type: 'Point',
       coordinates: [ row.field1, row.field2 ]
     }
   }
-}
+ } 
