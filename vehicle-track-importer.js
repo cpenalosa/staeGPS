e6fb26f295cf6656131a6cbbd46a9d6ac08c5c6f
cpenalosa <penalosa619@gmail.com>
2018-01-23 12:52:20 -0500

    Update vehicle-track-importer.js

--- vehicle-track-importer.js
+++ vehicle-track-importer.js
@@ -1,14 +1,16 @@
-// https://thingspeak.com/channels/401537/feed.csv
+// https://github.com/cpenalosa/testgps/blob/master/test-data.csv
  
 
-export default (row) => ({
-  if (row.entry_id == null || row.entry_id === 0) return null,
-  if (row.field1 == null || row.field1 === 0) return null,
-  if (row.field2 == null || row.field2 === 0) return null,
-  id: entry_id,
-  utc-time: created_at,
-  location: {
-    type: 'Point',
-    coordinates: [ row.field1, row.field2 ]
+export default (row) => {
+  if (row.entry_id == null || row.entry_id === 0) return null
+  if (row.field1 == null || row.field1 === 0) return null
+  if (row.field2 == null || row.field2 === 0) return null
+  return {
+    id: entry_id,
+    utc-time: created_at,
+    location: {
+      type: 'Point',
+      coordinates: [ row.field1, row.field2 ]
+    }
   }
-})
+}
